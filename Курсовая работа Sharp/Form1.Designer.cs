b60e95b339e6847399b57358345977958815412f
KoJli6ri <zadorojniy64@gmail.com>
2021-05-03 21:58:16 +0300

    first commit

---
+++ Курсовая работа Sharp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Курсовая_работа_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scorelabel1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scorelabel1
+            // 
+            this.scorelabel1.AutoSize = true;
+            this.scorelabel1.BackColor = System.Drawing.Color.Transparent;
+            this.scorelabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scorelabel1.Location = new System.Drawing.Point(759, 0);
+            this.scorelabel1.Name = "scorelabel1";
+            this.scorelabel1.Size = new System.Drawing.Size(25, 25);
+            this.scorelabel1.TabIndex = 0;
+            this.scorelabel1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Курсовая_работа_Sharp.Resource1.background;
+            this.ClientSize = new System.Drawing.Size(934, 531);
+            this.Controls.Add(this.scorelabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Soft";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scorelabel1;
+    }
+}
+
---
+++ Курсовая работа Sharp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Курсовая_работа_Sharp
+{
+    public partial class Form1 : Form
+    {   //картинки которые я использую на бэкграунде для выполнени поставленной задачи
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Bitmap HandlerTexutere = Resource1.handler1,
+                      TargetTexture = Resource1.target;
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player.URL = "Ace of Bace.mp3";
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            timer2.Interval = r.Next(25, 100);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+
+        //использую переменные для формы рисовки
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var LocalPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 5;
+            _targetPosition.Y += _direction.Y * 5;
+
+            if(_targetPosition.X < 0|| _targetPosition.X > 500)
+            {
+                _direction.X *= -1;
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(LocalPosition.X - _targetPosition.X, LocalPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt (between.X * between.X) + (between.Y * between.Y);   
+
+            if(distance <25)
+            {
+                AddScore(1);
+            }
+
+            var hendlerRect = new Rectangle(LocalPosition.X - 50, LocalPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(HandlerTexutere, hendlerRect);  
+            g.DrawImage(TargetTexture, targetRect);
+        }
+        private void AddScore(int score)
+        {
+            _score += score;
+            scorelabel1.Text = _score.ToString();
+        }
+
+    }
+}
